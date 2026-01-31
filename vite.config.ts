6b79cda28c01caf2113ac19493ca28c607bacf1c
Wessam-k <114925785+Wessam-K@users.noreply.github.com>
2026-01-31 19:33:41 +0100

    Update repository URL to Wk-Hub-Website

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Wk-Hub-Wesbite/',
+  base: '/Wk-Hub-Website/',
   server: {
     port: 8000,
     open: true
